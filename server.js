eccb7dd7156199b398799b5414ddd62c3a0f00b6
AlfredoGuacaran <alfredoguacaran21@gmail.com>
2022-02-14 22:53:01 -0300

    Ruta para procesamiento de img

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 app.use(express.static('static'));
 
+app.get('/procesarimg', (req, res) => {
+  res.send('Se esta procesando la imagen');
+});
+
 app.listen(3000, function () {
   console.log('Servidor andando en el puerto 3000');
 });
